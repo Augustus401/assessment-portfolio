224c764cac370a63e42fe782f3c4fda9acbc05f4
Augustus sales <augustusvidalsales@gmail.com>
2025-12-28 23:17:57 +0000

    loop

---
+++ .vscode/7some_counting.py
@@ -0,0 +1,19 @@
+print('counting up from 0 to 50')
+for a in range(1, 51):
+    print(a)
+print("---------------------------------------------------------------")
+print('counting down from 50 to 0')
+for b in range(50, 0, -1):
+    print(b)
+print("---------------------------------------------------------------")
+print('counting up from 30 to 50')
+for c in range(30, 51):
+    print(c)
+print("---------------------------------------------------------------")
+print('counting down from 50 to 10 by 2')
+for d in range(50, 9, -2):
+    print(d)
+print("---------------------------------------------------------------")
+print('counting up from 100 to 200 by 5')
+for e in range(100, 201, 5):
+    print(e)
